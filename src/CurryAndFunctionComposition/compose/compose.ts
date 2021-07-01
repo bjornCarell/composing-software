efb4aee7f48a5bf3c5251c8fe719555e28370332
Bjorn Carell <bjorn.carell@tink.com>
2021-07-01 23:21:53 +0200

    chore(CurryAndFunctionComposition): generic type vs Function epxlination

--- src/CurryAndFunctionComposition/compose/compose.ts
+++ src/CurryAndFunctionComposition/compose/compose.ts
@@ -2,5 +2,5 @@ export const compose = <T>(...fns: Function[]) => (x: T): T =>
   fns.reduceRight((y, f) => f(y), x);
 
 
-  // https://www.typescriptlang.org/docs/handbook/2/functions.html#unknown
-  // https://2ality.com/2018/04/type-notation-typescript.html#rest-parameters
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#unknown
+// https://2ality.com/2018/04/type-notation-typescript.html#rest-parameters
--- src/CurryAndFunctionComposition/pipe/pipe.ts
+++ src/CurryAndFunctionComposition/pipe/pipe.ts
@@ -1,3 +1,6 @@
 export const pipe = <T>(...fns: ((arg: T) => T)[]) =>
   (x: T) =>
     fns.reduce((y, f) => f(y), x);
+
+// Make use of generic type T to avoid using (...fns: Function[])
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#function
