946729d4fb58febd4bd9773452d2efcaaf3971b6
Bjorn Carell <bjorn.carell@tink.com>
2021-07-16 22:14:31 +0200

    feat(FunctorsAndCategories): Identity functor

--- src/FunctorsAndCategories/Identity/Identity.ts
+++ src/FunctorsAndCategories/Identity/Identity.ts
@@ -8,8 +8,22 @@ export const trace = <T>(x: T) => {
 }
 
 const u = Identity(2);
+
+// Identity law
 const r1 = u;
 const r2 = u.map(x => x);
 
-r1.map(trace);
-r2.map(trace);
+r1.map(trace); // 2
+r2.map(trace); // 2
+
+// Composition law
+const f = (n: number) => n + 1;
+const g = (n: number) => n * 2;
+
+const r3 = u.map(x => f(g(x)));
+const r4 = u.map(g).map(f);
+
+r3.map(trace); // 5
+r4.map(trace); // 5
+
+
